63a2e2226398483abe711551f21c8fedf5ec6a96
stevezmtstudios <me@stevezmt.top>
2026-09-04 12:21:46 +0800

    site config

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,27 +1,26 @@
 /**
  * Short Link Configuration Table.
  * Generated via admin.html
- */
+  */
 
 import { ShortLinkConfig } from '../types';
 
 export const config: ShortLinkConfig = {
   settings: {
-    domain: 'stevezmt.top',
+    domain: 'stev.cc',
     notFoundUrl: 'https://stevezmt.top/404?from=${FULL_URL}',
     notFoundMode: 'redirect',
-    defaultUtm: {
-      utm_source: 'shortlink',
-      utm_medium: 'redirect',
-    },
-    challenge: {
-      provider: 'cap',
-      siteKey: '8704cf7f-b00c-4801-a1bc-1572e585e623',
-    },
   },
 
   links: {
     '/': 'https://stevezmt.top',
     test: 'aes-gcm:v1:4Xbi9L4OgDE58ZziPf6TjO032BTv5uG9c2C4g6GqktYzrFwoFF8NGmBlsOJ77WVtm6hanvToxA==',
+    'blog:/*': 'https://blog.stevezmt.top/$1',
+    pubgpg: 'https://key.stevezmt.top',
+    'academic-abuse': 'https://link.stevezmt.top/avoid-political-disputes/index.html',
+    ethcalc: 'https://link.stevezmt.top/ethtool-advertise-bitmap-calc/index.html',
+    'blog/*': 'https://blog.stevezmt.top/$1',
+    rss: 'https://blog.stevezmt.top/atom.xml',
+    vcard: 'aes-gcm:v1:GZZdpmkW98GqPCYRkTUZ8UrSCvc6JoMqd0DoW3Gxqgqf73KG6qsBB7u15b5rnrPdNmMYwo/WCP0MQkUg/sTKMOXu5gEORdeK3PztInAkk/j7r1i7+YCH/UKppwLi',
   },
 };
